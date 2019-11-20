669194b1a7a8d0b65ed33822652a6c000ab85f05
YehuditAdler <53849407+YehuditAdler@users.noreply.github.com>
2019-11-20 07:17:26 +0200

    webhook Secret

--- src/main/java/com/exlibris/webhook/WebhookServlet.java
+++ src/main/java/com/exlibris/webhook/WebhookServlet.java
@@ -1,15 +1,23 @@
 package com.exlibris.webhook;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.net.util.Base64;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
+
+import com.exlibris.configuration.ConfigurationHandler;
 
 /**
  * Servlet implementation class MainServlet
@@ -17,41 +25,64 @@ import org.json.JSONObject;
 @WebServlet("/webhook")
 public class WebhookServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
-    final private static Logger logger = Logger.getLogger(WebhookServlet.class);
+	final Charset charSet = Charset.forName("UTF-8");
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String challenge = req.getParameter("challenge");
-        logger.info("challenge is :" + challenge);
-        resp.setContentType("application/json");
-        JSONObject json = new JSONObject();
-        json.put("challenge", challenge);
-        resp.getWriter().write(json.toString());
+	private static final long serialVersionUID = 1L;
+	final private static Logger logger = Logger.getLogger(WebhookServlet.class);
 
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String challenge = req.getParameter("challenge");
+		logger.info("challenge is :" + challenge);
+		resp.setContentType("application/json");
+		JSONObject json = new JSONObject();
+		json.put("challenge", challenge);
+		resp.getWriter().write(json.toString());
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+	}
 
-        String str;
-        String body = "";
-        while ((str = request.getReader().readLine()) != null) {
-            body += str;
-        }
-        logger.info("message is :" + body);
-        resp.getWriter().write("message is :" + body);
-        final String message = body;
-        
-        Runnable runner = new Runnable() {
-           public void run() {
-                ManageWebHookMessages.getWebhookMessage(message);
-            }
-        };
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
-        Thread thread = new Thread(runner);
-        thread.start();
-        logger.info("webhook handler ended");
+		JSONObject props = ConfigurationHandler.getInstance().getConfiguration();
+		String secret = props.get("webhook_secret").toString();
+		String signature = request.getHeader("X-Exl-Signature");
 
-    }
+		String str;
+		String body = "";
+		while ((str = request.getReader().readLine()) != null) {
+			body += str;
+		}
+		logger.info("message is :" + body);
+		resp.getWriter().write("message is :" + body);
+		final String message = body;
+		boolean validateSignature = false;
+		try {
+			validateSignature = validateSignature(message, secret, signature);
+		} catch (Exception e) {
+		}
+		if (!validateSignature) {
+			resp.getWriter().write("un validate signature");
+			logger.info("un validate signature");
+			return;
+		}
+		Runnable runner = new Runnable() {
+			public void run() {
+				ManageWebHookMessages.getWebhookMessage(message);
+			}
+		};
+
+		Thread thread = new Thread(runner);
+		thread.start();
+		logger.info("webhook handler ended");
+
+	}
+
+	private boolean validateSignature(String message, String secret, String signature) throws Exception {
+		Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+		SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+		sha256_HMAC.init(secret_key);
+		String hash = Base64.encodeBase64URLSafeString(sha256_HMAC.doFinal(message.getBytes())) + "=";
+		return hash.equals(signature);
+	}
 }
