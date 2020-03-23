634ac783231230e6051e3957f068d5b7b1468a4f
YehuditAdler <dittyadler@gmail.com>
2020-03-23 15:45:41 +0200

    Allow parallel handling of the item publishing output files. Default of 4 threads

--- src/main/java/com/exlibris/items/ItemsMain.java
+++ src/main/java/com/exlibris/items/ItemsMain.java
@@ -2,7 +2,12 @@ package com.exlibris.items;
 
 import java.io.File;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
@@ -17,14 +22,33 @@ import com.exlibris.ftp.FTPUtil;
 import com.exlibris.ftp.SFTPUtil;
 import com.exlibris.util.XmlUtil;
 
+class Task implements Runnable {
+
+    File xmlFile;
+    String institution;
+    Map<String, Integer> fileCounter;
+
+    public Task(File xmlFile, String institution, Map<String, Integer> fileCounter) {
+        this.xmlFile = xmlFile;
+        this.institution = institution;
+        this.fileCounter = fileCounter;
+    }
+
+    public void run() {
+        ItemsMain.hendleFile(xmlFile, institution, fileCounter);
+    }
+}
+
 public class ItemsMain {
 
     final static String MAINLOCALFOLDER = "files/items/";
+    static int maxNumberOfThreds = 4;
 
     final static Logger logger = Logger.getLogger(ItemsMain.class);
 
     public static synchronized void mergeItemsWithSCF(String institution) {
         FTPClient ftpUtil;
+
         try {
             JSONObject props = ConfigurationHandler.getInstance().getConfiguration();
             String ftpFolder = props.getJSONObject("ftp_server").getString("main_folder");
@@ -32,6 +56,9 @@ public class ItemsMain {
             String mainLocalFolder = MAINLOCALFOLDER;
             if (props.has("main_local_folder") && props.get("main_local_folder") != null) {
                 mainLocalFolder = props.getString("main_local_folder") + "/" + MAINLOCALFOLDER;
+            }
+            if (props.has("max_number_of_threds") && props.get("max_number_of_threds") != null) {
+                maxNumberOfThreds = Integer.valueOf(props.getString("max_number_of_threds"));
             }
             logger.info("Starting Merge Items With SCF For Institution: " + institution);
             logger.debug("empty the local folder");
@@ -55,41 +82,23 @@ public class ItemsMain {
             logger.debug("loop over xml files and convert them to records");
             File xmlFolder = new File(mainLocalFolder + "xml/");
             File[] xmlFiles = xmlFolder.listFiles();
-            int totalRecords = 0;
+            ExecutorService pool = Executors.newFixedThreadPool(maxNumberOfThreds);
+            Map<String, Integer> fileCounter = new HashMap<String, Integer>();
             for (File xmlFile : xmlFiles) {
-                String methodName = null;
-                if (xmlFile.getName().toLowerCase().contains("_delete")) {
-                    methodName = "itemDeleted";
-                } else {
-                    methodName = "itemUpdated";
+                logger.info("starting to hendle xml file: " + xmlFile.getName());
+                fileCounter.put(xmlFile.getName(), 0);
+                pool.execute(new Task(xmlFile, institution, fileCounter));
+            }
+            pool.shutdown();
+            try {
+                if (!pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
+                    pool.shutdownNow();
                 }
-                logger.debug("convert xml file to marc4j");
-                List<Record> records = XmlUtil.xmlFileToMarc4jRecords(xmlFile);
-                logger.debug("loop over records and merge with SCF");
-                for (Record record : records) {
-                    logger.debug("get network system number");
-                    String NZMmsId = getNetworkNumber(record.getVariableFields("035"));
-                    List<VariableField> variableFields = record.getVariableFields("ITM");
-                    for (VariableField variableField : variableFields) {
-                        ItemData itemData = ItemData.dataFieldToItemData(record.getControlNumber(),
-                                (DataField) variableField, institution, NZMmsId);
-                        try {
-                            if (itemData.getBarcode() == null) {
-                                logger.warn("Synchronize Item Failed. Barcode is null Item Data: " + variableField);
-                                continue;
-                            }
-                            if (itemData.getNetworkNumber() == null) {
-                                itemData.setRecord(record);
-                            }
-                            Method method = Class.forName("com.exlibris.items.ItemsHandler").getMethod(methodName,
-                                    ItemData.class);
-                            method.invoke(null, itemData);
-                        } catch (Exception e) {
-                            logger.error("Failed to handle item " + itemData.getBarcode(), e);
-                        }
-                    }
-                }
-                totalRecords += records.size();
+            } catch (InterruptedException e) {
+            }
+            int totalRecords = 0;
+            for (Integer count : fileCounter.values()) {
+                totalRecords = totalRecords += count;
             }
             logger.info("Total Records from FTP: " + totalRecords);
 
@@ -109,4 +118,44 @@ public class ItemsMain {
         return null;
     }
 
+    public static void hendleFile(File xmlFile, String institution, Map<String, Integer> fileCounter) {
+        try {
+            String methodName = null;
+            if (xmlFile.getName().toLowerCase().contains("_delete")) {
+                methodName = "itemDeleted";
+            } else {
+                methodName = "itemUpdated";
+            }
+            logger.debug("convert xml file to marc4j");
+            List<Record> records = XmlUtil.xmlFileToMarc4jRecords(xmlFile);
+            logger.debug("loop over records and merge with SCF");
+            for (Record record : records) {
+                logger.debug("get network system number");
+                String NZMmsId = getNetworkNumber(record.getVariableFields("035"));
+                List<VariableField> variableFields = record.getVariableFields("ITM");
+                for (VariableField variableField : variableFields) {
+                    ItemData itemData = ItemData.dataFieldToItemData(record.getControlNumber(),
+                            (DataField) variableField, institution, NZMmsId);
+                    try {
+                        if (itemData.getBarcode() == null) {
+                            logger.warn("Synchronize Item Failed. Barcode is null Item Data: " + variableField);
+                            continue;
+                        }
+                        if (itemData.getNetworkNumber() == null) {
+                            itemData.setRecord(record);
+                        }
+                        Method method = Class.forName("com.exlibris.items.ItemsHandler").getMethod(methodName,
+                                ItemData.class);
+                        method.invoke(null, itemData);
+                    } catch (Exception e) {
+                        logger.error("Failed to handle item " + itemData.getBarcode(), e);
+                    }
+                }
+            }
+            fileCounter.put(xmlFile.getName(), records.size());
+            logger.info("Total records from file: " + xmlFile.getName() + " :" + records.size());
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
 }
